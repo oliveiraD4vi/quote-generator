3485f0b6b524b0d2579d364d066db464e82fd3fa
Davi Oliveira <davi.comp.oliveira@gmail.com>
2023-02-14 09:56:46 -0300

    style(style): added background style

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,3 @@
 <script setup lang="ts"></script>
 
-<template>App</template>
+<template>Quote Generator</template>
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,5 @@ import { createApp } from "vue";
 import App from "./App.vue";
 
 import "./assets/scss/_global.scss";
-import "./assets/scss/_variables.scss";
 
 createApp(App).mount("#app");
